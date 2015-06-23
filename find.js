52977436e8636b98ed1385a03d0063b29af6f299
Josh Marinacci <joshua@marinacci.org>
2015-06-22 20:05:34 -0700

    updates

--- find.js
+++ find.js
@@ -59,7 +59,10 @@ var whitelist = [
 ];
 */
 var whitelist = [
-    'b8:3e:59'
+    'b8:27:eb', // rasberry pi ethernet
+    '00:e0:4c', // realtek
+    '00:14:78', // tp-link
+    '00:0c:43'  // ralink
 ];
 
 var interfaces = os.networkInterfaces();
