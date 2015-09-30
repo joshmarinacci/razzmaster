eb4943768ce6985c23367b4757ce2a34f9e73993
josh <joshua@marinacci.org>
2015-09-29 19:41:11 -0700

    code refactor support setting wifi

--- install.js
+++ install.js
@@ -42,37 +42,64 @@ function execRemote(conn, args) {
         return common.execRemote(conn,args);
     }
 }
+
+function processInstalls(config) {
 //update apt
-funcs.push(execRemote(conn, 'sudo apt-get -y update'));
-funcs.push(execRemote(conn, 'sudo apt-get -y upgrade'));
+    funcs.push(execRemote(conn, 'sudo apt-get -y update'));
+    funcs.push(execRemote(conn, 'sudo apt-get -y upgrade'));
 
 // install packages
-config.packages.forEach(function(pak){
-    if(pak == 'nodejs') {
-        //install node
-        funcs.push(execRemote(conn, 'curl -sL https://deb.nodesource.com/setup | sudo bash -'))
-        funcs.push(execRemote(conn, 'sudo apt-get install -y nodejs'));
-        funcs.push(execRemote(conn, 'node --version'));
-        funcs.push(execRemote(conn, 'npm --version'));
-    } else {
-        funcs.push(execRemote(conn, 'sudo apt-get install -y ' + pak));
+    if(config.packages) {
+        config.packages.forEach(function (pak) {
+            if (pak == 'nodejs') {
+                //install node
+                funcs.push(execRemote(conn, 'curl -sL https://deb.nodesource.com/setup | sudo bash -'))
+                funcs.push(execRemote(conn, 'sudo apt-get install -y nodejs'));
+                funcs.push(execRemote(conn, 'node --version'));
+                funcs.push(execRemote(conn, 'npm --version'));
+            } else {
+                funcs.push(execRemote(conn, 'sudo apt-get install -y ' + pak));
+            }
+        });
     }
-});
 
 // install global npms
-if(config.git) {
-    config.git.forEach(function(repo){
-        funcs.push(execRemote(conn,"cd $HOME"));
-        funcs.push(execRemote(conn,'git clone '+repo));
-    })
+    if(config.git) {
+        config.git.forEach(function(repo){
+            funcs.push(execRemote(conn,"cd $HOME"));
+            funcs.push(execRemote(conn,'git clone '+repo));
+        })
+    }
+
+    if(config.npm) {
+        config.npm.forEach(function(mod){
+            funcs.push(execRemote(conn, "npm install -g "+mod));
+        });
+    }
 }
 
-if(config.npm) {
-    config.npm.forEach(function(mod){
-        funcs.push(execRemote(conn, "npm install -g "+mod));
-    });
-}
+function processWifi(config) {
+    if(!config.wifi) return;
+    if(!config.wifi.ssid) return console.log("ssid missing!");
+    if(!config.wifi.password) return console.log("password missing!");
+    var ssid = config.wifi.ssid;
+    var pass = config.wifi.password;
+    var line1 = 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev';
+    var line2 = 'update_config=1';
+    var line3 = 'network={\n    ssid="'+ssid+'"\n    psk="'+pass+'" \n}';
+    var str = [line1,line2,line3].join("\n");
+    str = str.replace(/\"/g,"\\\"");
+    //console.log('string is ',str);
 
+    var filename = '/etc/wpa_supplicant/wpa_supplicant.conf';
+    funcs.push(execRemote(conn,'sudo echo "'+str+'" > tmp.txt'));
+    funcs.push(execRemote(conn,'sudo cp tmp.txt '+filename));
+    funcs.push(execRemote(conn,'sudo ifdown wlan0'));
+    funcs.push(execRemote(conn,'sudo ifup wlan0'));
+
+}
+processWifi(config);
+processInstalls(config);
 conn.on('ready', function() {
     common.chain(funcs).done(function(){
         conn.end();
